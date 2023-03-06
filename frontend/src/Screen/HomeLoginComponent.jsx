ccabb816dc3f8a4a65dd5da7612a2a03f64464e0
Anoop555777 <ajbisht99@gmail.com>
2023-03-06 20:02:02 +0530

    initial

---
+++ frontend/src/Screen/HomeLoginComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomeLoginComponent = () => {
+  return (
+    <>
+      <div className="login">
+        <div className="login__details"></div>
+        <div className="login__form"></div>
+      </div>
+    </>
+  );
+};
+
+export default HomeLoginComponent;
